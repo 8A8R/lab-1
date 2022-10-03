42f41594d5ca172993ec96bfd322df32aaceb1c4
Ahmed88r <ahmed.haj8r@mail.ru>
2022-10-04 00:44:01 +0300

    lab 1

--- main.py
+++ main.py
@@ -1,27 +1,67 @@
 import csv
+import random
 
-with open('books-en.csv') as csvfile:
-    table = csv.reader(csvfile, delimiter=';')
-    counter = 0
-    counter_greater_30 = 0
-    counter_years = 0
-    i = 0
 
-    for row in table:
-        if i == 0:
+def findByAuthor():
+    author = input('enter the name og an author:')
+    with open('books-en.csv') as csvfile:
+        table = csv.reader(csvfile, delimiter=';')
+
+        i = 0
+        results = []
+        for row in table:
+            if i == 0:
+                i += 1
+                continue
             i += 1
-            continue
-        i += 1
-        counter += 1
-        if len(row[1]) <= 30:
-            counter_greater_30 += 1
-        if 2016 <= int(row[3]) <= 2018:
-            counter_years += 1
+
+            if 2016 <= int(row[3]) <= 2018 and row[2] == author:
+                results.append(row[1])
+
+        if len(results) == 0:
+            print(f'there is no books the auther name {author} and between 2016 and 2018')
+        else:
+            print(f'The number of entries with year between 2016 and 2018 with the name {author}is {results}')
 
 
-# Question 1
-    print(f'The number of entries in the file is {counter}')
-# Question 2
-    print(f'The number of entries with Name greater than 30 is {counter_greater_30}')
-# Question 3
-print(f'The number of entries with year between 2016 and 2018 is {counter_years}')
+def countRecords():
+    with open('books-en.csv') as csvfile:
+        table = csv.reader(csvfile, delimiter=';')
+        counter = 0
+        for row in table:
+            counter += 1
+        print(f'The number of entries in the file is {counter}')
+
+
+def countRecordsMorethan30Let():
+    with open('books-en.csv') as csvfile:
+        table = csv.reader(csvfile, delimiter=';')
+
+        counter_greater_30 = 0
+        i = 0
+        for row in table:
+            if i == 0:
+                i+=1
+                continue
+            if len(row[1]) <= 30:
+                counter_greater_30 += 1
+        print(f'The number of entries with Name greater than 30 is {counter_greater_30}')
+
+
+def writeRandomBooks():
+    with open('books-en.csv') as csvfile:
+        table = csv.reader(csvfile, delimiter=';')
+        list1 = list(table)
+        randomValues = []
+
+        for i in range(20):
+            randomValues.insert(1, random.choice(list1))
+
+        with open('random-values.txt', 'w') as randomBooks:
+            i = 0
+            for line in randomValues:
+                i += 1
+                randomBooks.write(str(i) + "-" + line[1] + " , " + line[2] + " - " + line[3] + "\n")
+
+
+countRecordsMorethan30Let()
